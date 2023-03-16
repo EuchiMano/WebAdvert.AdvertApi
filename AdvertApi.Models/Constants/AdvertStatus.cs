332228a79621387b2dc12ae46a6efd3817aa4039
EuchiMano <marce.ps666@gmail.com>
2023-03-15 23:27:06 -0400

    Added dynamo dependencies

---
+++ AdvertApi.Models/Constants/AdvertStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace AdvertApi.Models.Constants;
+
+public enum AdvertStatus
+{
+	Pending = 1,
+	Active = 2
+}
---
+++ AdvertApi/Interfaces/IAdvertStorageService.cs
@@ -0,0 +1,9 @@
+﻿using AdvertApi.Models;
+
+namespace AdvertApi.Services;
+
+public interface IAdvertStorageService
+{
+	Task<string> Add(AdvertModel model);
+	Task<bool> Confirm(ConfirmAdvertModel model);
+}
